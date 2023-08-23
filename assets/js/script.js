00879d293381ff23467f4e737535b1a0b3eebd05
sweetsoph <sophia.verardods@gmail.com>
2023-08-23 15:37:42 -0300

    page portal do aluno to be finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,27 @@ var splash = document.getElementById("splash");
 var main = document.getElementById("main");
 
 function splashScreen() {
-    splash.style.display = "none";
-    main.style.display = "flex";
+  splash.style.display = "none";
+  main.style.display = "flex";
 }
 setTimeout(splashScreen, 1500);
 
 var btnNav = document.getElementById("button-nav");
 
 btnNav.addEventListener("click", function () {
-    document.getElementById("nav").classList.toggle("active");
+  document.getElementById("nav").classList.toggle("active");
 });
-
 
 var btnLogin = document.getElementById("btn-login");
 
 btnLogin.addEventListener("click", function () {
-    window.location.href = "screens/login/login.html";
+  window.location.href = "screens/login/login.html";
 });
 
-var btnSearchCPF = document.getElementById("btn-saiba-mais");
+var btnSearchCPF = document.getElementsByClassName("btn-saiba-mais");
 
-btnSearchCPF.addEventListener("click",()=>{
+for (i = 0; i < btnSearchCPF.length; i++) {
+  btnSearchCPF[i].addEventListener("click", () => {
     window.location.href = "screens/Login/consultarCPF.html";
-});
+  });
+}
